ca9f6e2284b10c4df6c6022f66c585d8c8e18e2e
Your Name <you@example.com>
2016-07-22 22:32:09 +0900

    update

--- scripts/production_server.sh
+++ scripts/production_server.sh
@@ -3,9 +3,9 @@
 GUNICORN=/root/.virtualenvs/poke/bin/gunicorn
 PROJECT_ROOT=/var/poke/pokemap
 
-APP=example:app
+APP=wsgi:app
 
 cd $PROJECT_ROOT
-exec $GUNICORN -c $PROJECT_ROOT/config/guniconf.py $APP
+exec $GUNICORN -c $PROJECT_ROOT/config/gunicorn/guniconf.py $APP
 
 
