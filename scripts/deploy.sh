67e6bb96f2129bd25e56b5a0376fa85f2a1ff7af
ikeda <ikeda@000196-M3.local>
2016-07-23 00:56:48 +0900

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,10 @@ set -eu
 echo "deploy start"
 
 for f in poke-app1 poke-app2; do
+  echo ""
+  echo "_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/"
   echo "$f"
+  echo "_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/"
   ssh -l root $f "date"
   ssh -l root $f "cd /var/poke/pokemap && git pull origin master"
   ssh -l root $f "/usr/bin/supervisorctl -c /etc/supervisord.conf restart gunicorn"
--- scripts/deploy_worker.sh
+++ scripts/deploy_worker.sh
@@ -5,7 +5,10 @@ set -eu
 echo "deploy start"
 
 for f in poke-worker1; do
+  echo ""
+  echo "_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/"
   echo "$f"
+  echo "_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/"
   ssh -l root $f "date"
   ssh -l root $f "cd /var/poke/pokemap && git pull origin master"
   ssh -l root $f "/usr/bin/supervisorctl -c /etc/supervisord.conf restart all"
