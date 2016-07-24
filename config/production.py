289b808e949f3c088e17010e7291e8b6863e1104
ikeda <ikeda@ikedashuuhei-no-iMac.local>
2016-07-24 12:02:05 +0900

    add production settings

--- config/production.py
+++ config/production.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from config.local import *
-REDIS_HOSTS = ['192.168.10.1']
+REDIS_HOSTS = ['192.168.10.1', '192.168.10.4', '192.168.10.5', '192.168.10.6', ]
 # GOOGLEMAPS_KEY = "AIzaSyA1Vav9DAf1028YRjPeZlXnoGHixzwP-WA"
 GOOGLEMAPS_KEY = "AIzaSyANot6E0gUmvYIgnlAY0I4-2PDRjKdxpwo"
 
