9a7a39e13d6b550dc9d5c1bc126c7d2ccb9d9dc8
ikeda <ikeda@ikedashuuhei-no-iMac.local>
2016-07-24 09:44:53 +0900

    fix import error

--- config/local.py
+++ config/local.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from config.base import *
-from config.password_local import *
+
+try:
+    from config.password_local2 import *
+except ImportError:
+    pass
 
