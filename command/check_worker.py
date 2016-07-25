0a13216702bda65a9d552ba2c85dff78078eda17
ikeda <ikeda@000196-M3.local>
2016-07-25 19:59:04 +0900

    update cw command

--- command/check_worker.py
+++ command/check_worker.py
@@ -17,9 +17,18 @@ class CheckWorker(Command):
         pass
 
     def _run(self):
+        ng_group = []
         for x in xrange((len(POINTS))):
             num = len(get_all_pokemon(point=x))
             if num > 5:
                 print "[+]ok({}) point:{}".format(num, x)
             else:
                 print "[-]NGNGNG({}) point:{}".format(num, x)
+                ng_group.append((num, x))
+
+        print("++++++++++++++++++++++++++++++++++++")
+        print("ERRORS")
+        print("++++++++++++++++++++++++++++++++++++")
+        for num, x in ng_group:
+            print "[-]NG({}) point:{}".format(num, x)
+        print("++++++++++++++++++++++++++++++++++++")
