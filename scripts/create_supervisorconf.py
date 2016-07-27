1ed0118082a9ac1235dcfbd3b75464eeb3bedd05
ikeda <ikeda@ikedashuuhei-no-iMac.local>
2016-07-28 00:20:26 +0900

    update sup ini

--- scripts/create_supervisorconf.py
+++ scripts/create_supervisorconf.py
@@ -20,7 +20,7 @@ redirect_stderr=true
 
 def create_file(ct):
     print("[+]start create file : {}".format(ct))
-    _base_path = "/Users/ikeda/noah/maps/config/supervisord/up/"
+    _base_path = "/Users/ikeda/python/maps/config/supervisord/up/"
     filename = "up{0:03d}".format(ct)
     body = F.format(filename, ct, filename)
     print(body)
