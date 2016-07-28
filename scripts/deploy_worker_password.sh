ade236dcd660d5afbfa1b7cdda8fc64291787f50
ikeda <ikeda@000196-M3.local>
2016-07-28 11:41:14 +0900

    deploy script fix

---
+++ scripts/deploy_worker_password.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# エラーなら停止
+set -eu
+
+echo "deploy start sup"
+
+# app1
+scp ./config/password.py poke-app1:/var/poke/pokemap/config/password.py
+echo "app1 finish"
+
+# app2
+scp ./config/password.py poke-app2:/var/poke/pokemap/config/password.py
+echo "app2 finish"
+
+# worker1
+scp ./config/password.py poke-worker1:/var/poke/pokemap/config/password.py
+echo "worker1 finish"
+
+# worker2
+scp ./config/password.py poke-worker2:/var/poke/pokemap/config/password.py
+echo "worker2 finish"
+
+# worker3
+scp ./config/password.py poke-worker3:/var/poke/pokemap/config/password.py
+echo "worker3 finish"
+
+# worker4
+scp ./config/password.py poke-worker4:/var/poke/pokemap/config/password.py
+echo "worker4 finish"
