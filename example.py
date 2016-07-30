3c317318b962bc40614213af13d3a5e2634aa43b
ikeda <ikeda@ikedashuuhei-no-iMac.local>
2016-07-31 08:27:30 +0900

    update

--- example.py
+++ example.py
@@ -682,7 +682,7 @@ def update_map(point):
 
             print('Completed: ' + str(
                 ((step+1) + pos * .25 - .25) / steplimit2 * 100) + '%')
-            time.sleep(2)
+            time.sleep(5)
 
         # for monitor
         update_pokemon_count(point, pokemon_count)
