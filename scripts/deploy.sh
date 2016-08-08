41b01eaf71315e0e1d1a530f339c52bec90193a0
ikeda <ikeda@ikedashuuhei-no-iMac.local>
2016-08-08 22:43:14 +0900

    ADのテンプレ構成を最適化

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,9 +9,9 @@ for f in poke-app1 poke-app2 poke-app3 poke-app4 poke-app5; do
   echo "_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/"
   echo "$f"
   echo "_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/"
-  ssh -l root $f "date"
-  ssh -l root $f "cd /var/poke/pokemap && git pull origin master"
-  ssh -l root $f "/usr/bin/supervisorctl -c /etc/supervisord.conf restart gunicorn"
+  ssh -l root $f "date" &
+  ssh -l root $f "cd /var/poke/pokemap && git pull origin master" &
+  ssh -l root $f "/usr/bin/supervisorctl -c /etc/supervisord.conf restart gunicorn" &
 done
 
 
